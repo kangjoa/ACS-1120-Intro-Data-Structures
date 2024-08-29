5c9bc3f4c4c6e813c91a68d3241558be95e4cb5d
kangjoa <joannalaikang@gmail.com>
2024-08-29 16:55:59 -0700

    Update comments

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,32 +1,17 @@
 import random
 import sys
-# read in the words file
+# read in the words file and store in a list
 with open('/usr/share/dict/words', 'r') as file:
     words = file.read().splitlines()
 
-# select a random set of words from the file and store in a data type
-
-
-# def get_five_words(words):
-#     random.shuffle(words)
-#     return words[:5]
-
-
-# show_me = get_five_words(words)
-# print(show_me)
-
 # Get the second word in the command line
 num_words = int(sys.argv[1])
-
-# print(type(num_words))
 
 # Take the number and then return the words in a "sentence"
 
 
 def sentence_maker(num_words):
-    # selected_words = words[:num_words]
     selected_words = random.sample(words, num_words)
-    # random.shuffle(selected_words)
     print(' '.join(selected_words))
 
 
