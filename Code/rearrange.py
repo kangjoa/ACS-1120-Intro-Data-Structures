e35441fd8532dc0d3a582adb11ed57a7cc39390b
kangjoa <joannalaikang@gmail.com>
2024-08-29 16:43:46 -0700

    Complete dictionary_words

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -2,7 +2,7 @@ import random
 import sys
 # Take some words and rearrange them randomly
 
-# Start on the second word in the command line
+# Start on the second word in the command line and return a list
 words = sys.argv[1:]
 
 random.shuffle(words)
