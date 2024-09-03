51e835bf5613a0fc709434d14aaa80a39d477c19
kangjoa <joannalaikang@gmail.com>
2024-09-03 13:56:51 -0700

    Include weights

--- Code/sample.py
+++ Code/sample.py
@@ -5,7 +5,7 @@ import random
 
 def sample(hist: histogram) -> str:
     """
-    Takes a histogram and returns a single word at random.
+    Takes a histogram and returns a single word at random, weighted by the frequency of the word.
 
     Args:
         hist (histogram): A histogram where keys are words and values are the number of times each word appears.
@@ -13,17 +13,27 @@ def sample(hist: histogram) -> str:
     Returns:
         str: Single random word.
     """
-    # Get the second word in the command line
-    file_path = str(sys.argv[1])
+    #  Get all the words (keys)
+    words = list(hist.keys())
 
-    random_word = random.choice(list(hist.keys()))
+    # Get all the weights (values/frequencies)
+    weights = list(hist.values())
+
+    # Use random.choices(), which allows weights
+    random_word = random.choices(words, weights=weights, k=1)[0]
 
     return random_word
 
 
 if __name__ == '__main__':
-    corpus_hist = histogram('corpus_sample.txt')
+    # Get the second word in the command line
+    file_path = str(sys.argv[1])
 
-    print_me = sample(corpus_hist)
+    # Generate the histogram from the provided file
+    hist = histogram(file_path)
 
-    print(print_me)
+    # Sample a random word from the histogram
+    random_word_weighted = sample(hist)
+
+    # Print the selected random word
+    print(random_word_weighted)
