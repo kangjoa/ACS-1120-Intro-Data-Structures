733491146ef3aaf97ec8abc8305c1f8762a740ec
kangjoa <joannalaikang@gmail.com>
2024-09-03 14:30:13 -0700

    Enable Flask

--- Code/app.py
+++ Code/app.py
@@ -1,17 +1,25 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
-
+from histogram import histogram
+from sample import sample
 
 app = Flask(__name__)
 
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
+# app.py
+
+# Initialize histogram by using the imported histogram function
+histogram_data = histogram('source_text.txt')
 
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+    # Sample a random word from the histogram
+    random_word_weighted = sample(histogram_data)
+
+    return f"<p>{random_word_weighted}</p>"
 
 
 if __name__ == "__main__":
